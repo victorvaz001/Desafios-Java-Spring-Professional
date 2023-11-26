66f3b06906e43c0e3460bb1a800de0432d3f1ce9
victorvaz001 <victorvaz001@gmail.com>
2023-11-26 17:09:19 -0300

    Project created

---
+++ Componentes e injeção de dependência/desafio/src/main/java/com/victor/desafio/DesafioApplication.java
@@ -0,0 +1,20 @@
+package com.victor.desafio;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Project Created!");
+
+	}
+}
