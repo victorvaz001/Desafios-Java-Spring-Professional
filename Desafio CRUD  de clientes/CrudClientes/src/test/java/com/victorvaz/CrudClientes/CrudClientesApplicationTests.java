2b9b7672f5c28882c1d7a3c620f54fabc2544cce
victorvaz001 <victorvaz001@gmail.com>
2023-12-12 16:12:31 -0300

    Project created

---
+++ Desafio CRUD  de clientes/CrudClientes/src/test/java/com/victorvaz/CrudClientes/CrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorvaz.CrudClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
